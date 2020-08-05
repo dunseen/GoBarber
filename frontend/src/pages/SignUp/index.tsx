4d84719c87848d4c1a6838af67819104f4303733
Davys lima <davysjunior08@hotmail.com>
2020-08-05 06:37:34 -0300

    Exibindo erros no input, finalizado.

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import React, { useCallback, useRef } from "react";
 import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
 import * as Yup from "yup";
+import getValidationErros from "../../utils/getValidationErrors";
 
 import logoImg from "../../assets/logo.svg";
 
@@ -16,6 +18,8 @@ const SignUp: React.FC = () => {
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required("Nome obrigatório"),
         email: Yup.string()
@@ -28,10 +32,8 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log("entrei");
-      formRef.current?.setErrors({
-        name: "Nome Obrigatório",
-      });
+      const errors = getValidationErros(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
   return (
@@ -40,7 +42,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu Cadastro</h1>
 
           <Input name="nome" icon={FiUser} placeholder="Nome" />
